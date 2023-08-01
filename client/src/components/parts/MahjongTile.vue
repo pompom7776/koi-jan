8cc749607076c0dc58eceb3f80027e755dfb3f31
KITA-DS12 <kita.ds12@gmail.com>
2023-08-01 23:06:01 +0900

    feature: display tile-name hover

--- client/src/components/parts/MahjongTile.vue
+++ client/src/components/parts/MahjongTile.vue
@@ -6,6 +6,45 @@ const tile = props.tile;
 const scale = props.scale;
 const rotate = props.rotate;
 
+const hovered = ref(false);
+
+const tileNames = {
+  "1m": "社長",
+  "2m": "恋人",
+  "3m": "愛人",
+  "4m": "有名人",
+  "5m": "幼馴染",
+  "6m": "友達",
+  "7m": "初対面",
+  "8m": "先輩",
+  "9m": "先生",
+  "1p": "お金",
+  "2p": "浴衣",
+  "3p": "香水",
+  "4p": "サングラス",
+  "5p": "ゲーム",
+  "6p": "学生証",
+  "7p": "スマホ",
+  "8p": "酒",
+  "9p": "メガネ",
+  "1s": "高級レストラン",
+  "2s": "お祭り",
+  "3s": "ホテル",
+  "4s": "個室焼肉",
+  "5s": "おうち",
+  "6s": "カラオケ",
+  "7s": "カフェ",
+  "8s": "居酒屋",
+  "9s": "ドライブ",
+  east: "好き",
+  south: "愛してる",
+  west: "かわいい",
+  north: "かっこいい",
+  white: "デート行きませんか",
+  green: "付き合ってください",
+  red: "手繋ご？",
+};
+
 const size = computed(() => ({
   width: `${4.3 * scale}vw`,
   height: `${5 * scale}vw`,
@@ -36,10 +75,11 @@ const getTileClass = (tile) => {
 };
 
 const tileStyle = ref({});
-const handleTileHover = (hovered) => {
-  if (hovered) {
+const handleTileHover = (isHovered) => {
+  hovered.value = isHovered;
+  if (isHovered) {
     tileStyle.value = {
-      transform: `rotate(${rotate}deg) translateY(-10px)`,
+      transform: `rotate(${rotate}deg) translateY(-1.2vw)`,
       transition: "0.3s",
       cursor: "pointer",
     };
@@ -60,8 +100,9 @@ const getImagePath = (tile) => {
     @mouseover="handleTileHover(true)" @mouseleave="handleTileHover(false)">
     {{ tile }}
   </div>
-  <div v-else :class="getTileClass(tile)" :style="{ ...getStyle, ...tileStyle }" @mouseover="handleTileHover(true)"
+  <div v-else class="getTileClass(tile)" :style="{ ...getStyle, ...tileStyle }" @mouseover="handleTileHover(true)"
     @mouseleave="handleTileHover(false)">
+    <div v-show="hovered" class="tile-value">{{ tileNames[tile] }}</div>
     <img :style="{ ...getStyle }" :src="getImagePath(tile)" />
   </div>
 </template>
@@ -75,11 +116,26 @@ const getImagePath = (tile) => {
   writing-mode: vertical-rl;
   background-color: white;
   color: black;
+  position: relative;
 }
 
 .back-tile {
   pointer-events: none;
   color: orange;
   background-color: orange;
+}
+
+.tile-value {
+  position: absolute;
+  top: -75%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  white-space: nowrap;
+  background-color: pink;
+  color: white;
+  border-radius: 50%;
+  padding: 10px;
+  border: 2px solid white;
 }
 </style>
