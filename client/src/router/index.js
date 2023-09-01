7869f05e875263596c498bdf7728581a8761c605
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2023-09-02 03:02:14 +0900

    home-page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Room from "../views/Room.vue";
 import WaitingRoom from "../views/WaitingRoom.vue";
 import Game from "../views/Game.vue";
+import Home from "../views/Home.vue"
 
 const routes = [
   { path: "/", component: Room },
   { path: "/room/:roomId/waiting", component: WaitingRoom },
   { path: "/room/:roomId/game", component: Game },
+  { path: "/Home", component: Home },
 ];
 
 const router = createRouter({
